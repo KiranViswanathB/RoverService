c7cd2a212601ba7e713d69ac941a0bf09ba8c061
Kiran Viswanath Balusu <kiranvb.dev@gmail.com>
2024-04-16 08:08:48 -0500

    Implement `NasaService`

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
+using RoverService.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-var app = builder.Build();
-
 builder.Services.AddHttpClient("NasaApi", httpClient =>
 {
     httpClient.BaseAddress = new Uri(builder.Configuration.GetSection("NasaApi")?.GetSection("BaseUrl")?.Value ?? "");
 });
+
+builder.Services.AddScoped<INasaService, NasaService>();
+
+var app = builder.Build();
 
 app.UseAuthorization();
 
